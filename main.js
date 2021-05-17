e08a3b7271d99a896c6a18f4ced24f62852fc7a5
LukeTunstallll <luketunstallll@hotmail.com>
2021-05-17 15:42:54 +0100

    Pro_Calculator

--- main.js
+++ main.js
@@ -1,66 +1,74 @@
 const clearDisplay = document.querySelector(".button__clear");
 
+let numOneResult = null;
+let numTwoResult = null;
+let operatorVal = null;
+
 clearDisplay.addEventListener("click", (event) => {
   calcDisplay.innerHTML = null;
+  let numOneResult = null;
+  let numTwoResult = null;
+  let operatorVal = null;
 });
 
 const calcDisplay = document.getElementById("idresult");
-
 const buttonPress = document.querySelectorAll(".numInput");
+const operatorPress = document.querySelectorAll(".operator");
+
+const equalPress = document.querySelector(".button__equals");
 
 buttonPress.forEach((number) => {
   number.addEventListener("click", (event) => {
-    console.log(event);
-    calcDisplay.innerHTML += buttonPress[number];
+    calcDisplay.innerHTML += number.dataset.numvalue;
   });
 });
 
-// buttonPress[0].addEventListener("click", (event) => {
-//   console.log(event);
-//   calcDisplay.innerHTML += 9;
-// });
+operatorPress.forEach((operator) => {
+  operator.addEventListener("click", (event) => {
+    numOneResult = parseInt(calcDisplay.innerHTML);
+    calcDisplay.innerHTML = "";
+    switch (operator.dataset.bob) {
+      case "0":
+        operatorVal = 0;
+        break;
+      case "1":
+        operatorVal = 1;
+        break;
+      case "2":
+        operatorVal = 2;
+        break;
+      case "3":
+        operatorVal = 3;
+        break;
+    }
+  });
+});
 
-// buttonPress[1].addEventListener("click", (event) => {
-//   console.log(event);
-//   calcDisplay.innerHTML += 8;
-// });
-
-// buttonPress[2].addEventListener("click", (event) => {
-//   console.log(event);
-//   calcDisplay.innerHTML += 7;
-// });
-
-// buttonPress[3].addEventListener("click", (event) => {
-//   console.log(event);
-//   calcDisplay.innerHTML += 6;
-// });
-
-// buttonPress[4].addEventListener("click", (event) => {
-//   console.log(event);
-//   calcDisplay.innerHTML += 5;
-// });
-
-// buttonPress[5].addEventListener("click", (event) => {
-//   console.log(event);
-//   calcDisplay.innerHTML += 4;
-// });
-
-// buttonPress[6].addEventListener("click", (event) => {
-//   console.log(event);
-//   calcDisplay.innerHTML += 3;
-// });
-
-// buttonPress[7].addEventListener("click", (event) => {
-//   console.log(event);
-//   calcDisplay.innerHTML += 2;
-// });
-
-// buttonPress[8].addEventListener("click", (event) => {
-//   console.log(event);
-//   calcDisplay.innerHTML += 1;
-// });
-
-// buttonPress[9].addEventListener("click", (event) => {
-//   console.log(event);
-//   calcDisplay.innerHTML += 0;
-// });
+equalPress.addEventListener("click", (event) => {
+  numTwoResult = parseInt(calcDisplay.innerHTML);
+  console.log(numTwoResult);
+  switch (operatorVal) {
+    case 0:
+      let addResult = numOneResult + numTwoResult;
+      calcDisplay.innerHTML = addResult.toString();
+      console.log(addResult);
+      break;
+    case 1:
+      let subResult = numOneResult - numTwoResult;
+      calcDisplay.innerHTML = subResult.toString();
+      console.log(subResult);
+      break;
+    case 2:
+      let timesResult = numOneResult * numTwoResult;
+      calcDisplay.innerHTML = timesResult.toString();
+      console.log(timesResult);
+      break;
+    case 3:
+      let divResult = numOneResult / numTwoResult;
+      calcDisplay.innerHTML = divResult.toString();
+      console.log(divResult);
+      break;
+    default:
+      calcDisplay.innerHTML = "ERROR";
+  }
+});
